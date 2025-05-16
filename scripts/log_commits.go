d9e6afa09a859b4c9e1cfab1406f2bec4f730cfd
llllluis <85310283+llllluis@users.noreply.github.com>
2025-05-16 17:20:35 +0200

    Update log_commits.go
    solventar errores

--- scripts/log_commits.go
+++ scripts/log_commits.go
@@ -1,44 +1,73 @@
 package main
-
+ 
 import (
+
   "fmt"
+
   "os"
+
   "os/exec"
+
   "path/filepath"
+
   "time"
+
 )
-
+ 
 func main() {
-  cmd := exec.Commnad("git","log","-n","3","--pretty=format:%h -%an, : %s"
+
+  cmd := exec.Command("git", "log", "-n", "3", "--pretty=format:%h - %an : %s")
+
   out, err := cmd.Output()
+
   if err != nil {
-      fmt.Printf("Error ejecutando git log %v\n", err)
-      os.Exit(1)
-    }
 
-  //creacio carpeta log
-  logDir := filepath.Join("..","log")
-  if _, err := os.Stat(logDir); os.IsNotExist(err) {
-    //definim permisos d'escriptura
-    err := os.Mkdir(logDir, 0755)
-    if err != nil {
-      fmt.Printf("Error creando directorio %s: %v\n", logDir, err)
-      os.Exit(1)
-    }
-  }
+    fmt.Printf("Error ejecutandogit log %v\n",err)
 
-  //generar nom arxiu
-  currentTime := time.Now().Format("2006-01-02_15:04:05") //Mascara de YYYY-mm-DD HH-ii-ss
-  logfile := filepath.Join(logDir,fmt.Sprintf("Commits_%s.txt",currentTime)
-
-  //escriure arxiu
-  err = fmt.Sprintf("S'han escrit els últim 3 commits del repositori:\n%s",string(out))
-  if err != nil {
-    fmt.Printf("S'ha produit un errer creant en %s %v\n", logfile,err)
     os.Exit(1)
-  }
 
-  fmt.Printf("S'ha creat l'arxiu log en %s\n",logfile)
+  }
+ 
+  //Creació de la carpeta log
+
+  logDir := filepath.Join("..", "log")
+
+  if _, err := os.Stat(logDir); os.IsNotExist(err) {
+
+    //Definim permisos d'escriptura
+
+    err = os.Mkdir(logDir, 0755)
+
+    if err != nil {
+
+      fmt.Printf("Error creando directorio %s: %v\n",logDir,err);
+
+      os.Exit(1)
+
+    }
+
+  }
+ 
+  //Generar nom de l'arxiu
+
+  currentTime := time.Now().Format("2006-01-02_15:04:05") //Mascara de YYYY-mm-dd HH-ii-ss
+
+  logFile := filepath.Join(logDir,fmt.Sprintf("commits_%s.txt",currentTime))
+ 
+  //Escriure l'arxiu
+
+  err := fmt.Sprintf("S'han escrit els ultims 3 commits del repositori:\n%s",string(out))
+
+  if err != nil {
+
+    fmt.Printf("S'ha produit un error creant en %s %v\n",logFile,err)
+
+    os.Exit(1)
+
+  }
+ 
+  fmt.Printf("S'ha creat l'arxiu de log en %s\n",logFile)
+
 }
-                      
-                      
+                 
+                    
