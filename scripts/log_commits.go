56959331038f4c8383aa45926a4298e50445345e
llllluis <85310283+llllluis@users.noreply.github.com>
2025-05-16 17:46:09 +0200

    Update log_commits.go

--- scripts/log_commits.go
+++ scripts/log_commits.go
@@ -1,73 +1,43 @@
 package main
  
 import (
-
   "fmt"
-
   "os"
-
   "os/exec"
-
   "path/filepath"
-
   "time"
-
 )
  
 func main() {
-
   cmd := exec.Command("git", "log", "-n", "3", "--pretty=format:%h - %an, %ar : %s")
-
   out, err := cmd.Output()
-
   if err != nil {
-
     fmt.Printf("Error ejecutandogit log %v\n",err)
-
     os.Exit(1)
-
   }
  
   //Creació de la carpeta log
-
   logDir := filepath.Join("..", "log")
-
   if _, err := os.Stat(logDir); os.IsNotExist(err) {
-
     //Definim permisos d'escriptura
-
     err = os.Mkdir(logDir, 0755)
-
     if err != nil {
-
       fmt.Printf("Error creando directorio %s: %v\n",logDir,err);
-
       os.Exit(1)
-
     }
-
   }
  
   //Generar nom de l'arxiu
-
   currentTime := time.Now().Format("2006-01-02_15:04:05") //Mascara de YYYY-mm-dd HH-ii-ss
-
   logFile := filepath.Join(logDir,fmt.Sprintf("commits_%s.txt",currentTime))
  
   //Escriure l'arxiu
-
   contingut := fmt.Sprintf("S'han escrit els ultims 3 commits del repositori:\n%s",string(out))
-  err : = os.WriteFile(logfile, []byte(contingut), 0644)
+  err = os.WriteFile(logFile, []byte(contingut), 0644)
   if err != nil {
-
     fmt.Printf("S'ha produit un error creant en %s %v\n",logFile,err)
-
     os.Exit(1)
-
   }
  
   fmt.Printf("S'ha creat l'arxiu de log en %s\n",logFile)
-
 }
-                 
-                    
